fd55e1e04fcaa3c6a7abb4d576c729f3ec69e2d2
0xkraken <hien.n@autonomous.nyc>
2020-04-01 18:33:28 +0700

    update bnb relayer

--- agents/bnbrelayer.go
+++ agents/bnbrelayer.go
@@ -8,6 +8,7 @@ import (
 	"github.com/tendermint/tendermint/rpc/client"
 	"github.com/tendermint/tendermint/types"
 	"portalfeeders/entities"
+	"time"
 )
 
 type BNBRelayer struct {
@@ -19,23 +20,27 @@ func (b *BNBRelayer) getLatestBNBBlockHeightFromIncognito() (int64, error) {
 	var relayingBlockRes entities.RelayingBlockRes
 	err := b.RPCClient.RPCCall("getlatestbnbheaderblockheight", nil, &relayingBlockRes)
 	if err != nil {
+		fmt.Errorf("Error when call RPC to get latest bnb header block height %v", err)
 		return int64(0), err
 	}
 	if relayingBlockRes.RPCError != nil {
+		fmt.Errorf(relayingBlockRes.RPCError.Message)
 		return int64(0), errors.New(relayingBlockRes.RPCError.Message)
 	}
 
 	res := relayingBlockRes.Result.(map[string]interface{})
 	latestBNBHeaderBlockHeight, ok := res["LatestBNBHeaderBlockHeight"]
 	if !ok {
+		fmt.Errorf("Can not get LatestBNBHeaderBlockHeight in response")
 		return int64(0), errors.New("Can not get LatestBNBHeaderBlockHeight in response")
 	}
-	latestBNBHeaderBlockHeightInt64, ok := latestBNBHeaderBlockHeight.(int64)
+	latestBNBHeaderBlockHeightFloat64, ok := latestBNBHeaderBlockHeight.(float64)
 	if !ok {
-		return int64(0), errors.New("Can not get latestBNBHeaderBlockHeightInt64 in response")
+		fmt.Errorf("Can not get latestBNBHeaderBlockHeightFloat64 in response")
+		return int64(0), errors.New("Can not get latestBNBHeaderBlockHeightFloat64 in response")
 	}
 
-	return latestBNBHeaderBlockHeightInt64, nil
+	return int64(latestBNBHeaderBlockHeightFloat64), nil
 }
 
 // getBNBBlockFromBNBChain calls RPC to get bnb block with blockHeight from BNB peers
@@ -72,18 +77,37 @@ func buildBNBHeaderStr (block *types.Block) (string, error) {
 }
 
 
-
 func (b *BNBRelayer) relayBNBBlockToIncognito(
 	bnbBlockHeight int64,
 	headerBlockStr string,
 ) error {
-	privateKey := "112t8rnjeorQyyy36Vz5cqtfQNoXuM7M2H92eEvLWimiAtnQCSZiP2HXpMW7mECSRXeRrP8yPwxKGuziBvGVfmxhQJSt2KqHAPZvYmM1ZKwR" // TODO: figure out to make it secret
+	//privateKey := "112t8rnjeorQyyy36Vz5cqtfQNoXuM7M2H92eEvLWimiAtnQCSZiP2HXpMW7mECSRXeRrP8yPwxKGuziBvGVfmxhQJSt2KqHAPZvYmM1ZKwR" // TODO: figure out to make it secret
+	//
+	//txID, err := CreateAndSendTxRelayBNBBlock(b.RPCClient, privateKey, headerBlockStr, bnbBlockHeight)
+	//if err != nil {
+	//	return err
+	//}
+	//fmt.Printf("relayBNBBlockToIncognito success with TxID: %v\n", txID)
 
-	txID, err := CreateAndSendTxRelayBNBBlock(b.RPCClient, privateKey, headerBlockStr, bnbBlockHeight)
+	meta := map[string]interface{}{
+		"SenderAddress": "12RuEdPjq4yxivzm8xPxRVHmkL74t4eAdUKPdKKhMEnpxPH3k8GEyULbwq4hjwHWmHQr7MmGBJsMpdCHsYAqNE18jipWQwciBf9yqvQ",
+		"Header":        headerBlockStr,
+		"BlockHeight":   bnbBlockHeight,
+	}
+	privateKey := "112t8rnjeorQyyy36Vz5cqtfQNoXuM7M2H92eEvLWimiAtnQCSZiP2HXpMW7mECSRXeRrP8yPwxKGuziBvGVfmxhQJSt2KqHAPZvYmM1ZKwR"
+	params := []interface{}{
+		privateKey, nil, -1, 0, meta,
+	}
+	var relayingBlockRes entities.RelayingBlockRes
+	err := b.RPCClient.RPCCall("createandsendtxwithrelayingbnbheader", params, &relayingBlockRes)
 	if err != nil {
+		fmt.Errorf("Error when call RPC create and send relaying bnb header %v\n", err)
 		return err
 	}
-	fmt.Printf("relayBNBBlockToIncognito success with TxID: %v\n", txID)
+	if relayingBlockRes.RPCError != nil {
+		fmt.Errorf("%v\n", relayingBlockRes.RPCError.Message)
+		return errors.New(relayingBlockRes.RPCError.Message)
+	}
 	return nil
 }
 
@@ -102,6 +126,7 @@ func (b *BNBRelayer) Execute() {
 	// get latest BNB block from Incognito
 	latestBNBBlockHeight, err := b.getLatestBNBBlockHeightFromIncognito()
 	if err != nil {
+		fmt.Printf("getLatestBNBBlockHeightFromIncognito error: %v\n", err)
 		return
 	}
 	nextBlockHeight := latestBNBBlockHeight + 1
@@ -110,19 +135,24 @@ func (b *BNBRelayer) Execute() {
 		// get next BNB block from BNB chain
 		block, err := b.getBNBBlockFromBNBChain(nextBlockHeight)
 		if err != nil {
+			fmt.Printf("getBNBBlockFromBNBChain error: %v\n", err)
 			break
 		}
 		headerBlockStr, err := buildBNBHeaderStr(block)
 		if err != nil {
+			fmt.Printf("buildBNBHeaderStr error: %v\n", err)
 			break
 		}
 
 		// relay next BNB block to Incognito
 		err = b.relayBNBBlockToIncognito(nextBlockHeight, headerBlockStr)
 		if err != nil {
+			fmt.Printf("relayBNBBlockToIncognito error: %v\n", err)
 			break
 		}
+		fmt.Printf("Relay bnb block header %v\n", nextBlockHeight)
 
 		nextBlockHeight++
+		time.Sleep(60000 * time.Millisecond)
 	}
 }
--- agents/constants.go
+++ agents/constants.go
@@ -11,8 +11,6 @@ const (
 	BNBMainnetAddress = "https://seed1.longevito.io:443"
 
 	// address of a bnb peer on testnet
-	BNBTestnetAddress = "http://localhost:26657"
-
-	//
-
+	//BNBTestnetAddress = "http://localhost:26657"
+	BNBTestnetAddress = "https://data-seed-pre-0-s1.binance.org:443"
 )
